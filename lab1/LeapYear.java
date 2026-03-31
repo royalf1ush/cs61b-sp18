b642aa366036d48f1843e4a8636664a5a93e4087
royalflush <jingyi_yang2005@hotmail.com>
2026-03-31 16:46:00 +0800

    completed whole part of lab1

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -1,5 +1,5 @@
 /** Class that determines whether or not a year is a leap year.
- *  @author YOUR NAME HERE
+ *  @author royalflush
  */
 public class LeapYear {
 
@@ -27,7 +27,16 @@ public class LeapYear {
             } catch (NumberFormatException e) {
                 System.out.printf("%s is not a valid number.\n", args[i]);
             }
+   
         }
     }
+    /** Receive a year and check if it is a leap year or not.*/
+    public static boolean isLeapYear(int year){
+    	if(year%400==0 || (year%4==0 && year%100!=0)){
+    		return true;
+	}else{
+		return false;
+	}
+   }
 }
 
