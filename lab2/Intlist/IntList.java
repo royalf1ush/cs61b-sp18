427b969e844e252e34f7d73ebeecadd4c9bda2b5
royalflush <jingyi_yang2005@hotmail.com>
2026-04-12 10:26:45 +0800

    completed whole part of lab2

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -81,8 +81,13 @@ public class IntList {
      */
 
     public static IntList dcatenate(IntList A, IntList B) {
-        //TODO:  fill in method
-        return null;
+        IntList ptr=A;
+        while (ptr.rest!=null)
+        {
+            ptr=ptr.rest;
+        }
+        ptr.rest=B;
+        return A;
     }
 
     /**
@@ -90,8 +95,11 @@ public class IntList {
      * * elements of B.  May NOT modify items of A.  Use 'new'.
      */
     public static IntList catenate(IntList A, IntList B) {
-        //TODO:  fill in method
-        return null;
+        if (A==null)
+        {
+            return B;
+        }
+        return new IntList(A.first,catenate(A.rest,B));
     }
 
 
